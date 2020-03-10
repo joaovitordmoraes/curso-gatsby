e17affd09975251e0751f875b142e44323a2a7d2
João Moraes <joaovitordmoraes@gmail.com>
2020-03-10 18:43:32 -0300

    :construction: Create the initial structure of posts in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,20 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import PostItem from "../components/PostItem"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+      <PostItem
+        slug="/about/"
+        background=""
+        category="Misc"
+        date="06 de Março de 2020"
+        timeToRead="5"
+        title="Fazendo meu Portfólio em Gatsby"
+        description="Aprendendo a fazer meu portfólio em Gatsby"
+      />
   </Layout>
 )
 
